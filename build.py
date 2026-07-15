b42bdf7caaaf525bd06af244c9861037c7f0af4a
pathoftav <pathoftav@proton.me>
2026-07-15 17:09:08 -0400

    update site.webmanifest

--- build.py
+++ build.py
@@ -62,7 +62,7 @@ PAGE = """\
 <link rel="apple-touch-icon" sizes="180x180" href="{root}static/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="{root}static/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="{root}static/favicon/favicon-16x16.png">
-<link rel="manifest" href="{root}static/site.webmanifest">
+<link rel="manifest" href="{root}static/favicon/site.webmanifest">
 <script>
 /* Apply theme before first paint to prevent flashes.
    Syncs with OS preference or localStorage override. */
